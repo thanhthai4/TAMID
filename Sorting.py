7fa720aee3e57fc53fa08ba7144982eb17107202
Thanh Thai <108910534+thanhthai4@users.noreply.github.com>
2022-09-17 22:44:15 -0400

    Update Sorting.py

--- Sorting.py
+++ Sorting.py
@@ -2,6 +2,6 @@ array1= [1, 3, 5, 8]
 array2 = [1, 2, 3]
 k = 5
 #This function joins the 2 arrays into one array 
-#sort it & take the first k numbers
+#sorts it & takes the first k numbers
 sortedArrays = sorted([*array1, *array2])[:k]
 print(sortedArrays)
